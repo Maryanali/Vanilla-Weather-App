fc3500710de3a8bb84afdd9a7d5a69058ca1bc00
Maryanali <73916090+Maryanali@users.noreply.github.com>
2021-02-18 00:18:25 +0000

    The javascript has been written for the forecast and the HTML, CSS has been updated too. This is now the completed weather forecast.

--- script.js
+++ script.js
@@ -1,15 +1,3 @@
-//let apiKey = "ac254995f1530b05133bdf3b89d170a4";
-//let city= "London";
-//let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric`;
-//let lat =position.coords.latitude;
-//let long =position.coords.longitude;
-//let apiCood= `https://api.openweathermap.org/data/2.5/onecall?lat=${position.coords.latitude}&lon=${position.coords.longitude}&exclude={part}&appid=ac254995f1530b05133bdf3b89d170a4`;
-//from the axios github, makes it easy to send HTTP requests 
-
-
-
-
-
 function formatDate(timestamp){
 
 let date = new Date(timestamp);
@@ -25,6 +13,19 @@ let days =["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Sa
 let day = days[date.getDay()];
 return `${day} ${hours}:${minutes}`;
 
+}
+
+function formatHours(timestamp){
+let date = new Date(timestamp);
+let hours = date.getHours();
+if (hours < 10){
+    hours = `0${hours}`;
+} 
+let minutes= date.getMinutes();
+if (minutes < 10){
+    minutes = `0${minutes}`;
+} 
+  return `${hours}:${minutes}`;
 }
 
 function displayTemperature(response){
@@ -63,90 +64,42 @@ cityElement.innerHTML = response.data.name;
 
 
 
+function displayForecast(response) {
+  let forecastElement = document.querySelector("#forecast");
+  forecastElement.innerHTML = null;
+  let forecast = null;
 
-
-//let h2 = document.querySelector("#city");
-//h2.innerHTML = city; 
-/*
-function showPosition(position){
-    let h1 = document.querySelector("h1");
-    h1.innerHTML= `Your Latitude is ${position.coords.latitude} and your longitude is ${position.coords.longitude}`;
-     
-} 
-
-function getCurrentPosition(){
-navigator.geolocation.getCurrentPosition(showPosition);
-}
-*/
-
-//This is console logging the temperature of Birmingham but this should
-//be initiated by a click of a button
-
-
-
-
-
-/*
-function requirePosition(position) {
-        let latitude = position.coords.latitude;
-        let longitude =position.coords.longitude;
-        let apiKey = "ac254995f1530b05133bdf3b89d170a4";
-        let apiUrl= `http://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${apiKey}&units=metric`;
-        axios.get(apiUrl).then(showTemperature);
+  for (let index = 0; index < 12; index++) {
+    forecast = response.data.list[index];
+    forecastElement.innerHTML += `
+    <div class="col-2">
+      <h3>
+        ${formatHours(forecast.dt * 1000)}
+      </h3>
+      <img
+        src="http://openweathermap.org/img/wn/${
+          forecast.weather[0].icon
+        }@2x.png"
+      />
+      <div class="weather-forecast-temperature">
+        <strong>
+          ${Math.round(forecast.main.temp_max)}°
+        </strong>
+        ${Math.round(forecast.main.temp_min)}°
+      </div>
+    </div>
+  `;
+  }
 }
 
-    navigator.geolocation.getCurrentPosition(requirePosition);
-
-function citySearch(event) {
-  event.preventDefault();
-  let formValue = document.querySelector("#search-form"); //we got the form where they write the city
-  let inputValue = document.querySelector("search-input"); //now we got the actual input on the form
-  let heading = document.querySelector("#city-search");
-
-//if the value of the input is there, then we can display it else do an alert
- if (inputValue.value){
-    city.innerHTML = `${inputValue.value}`; 
- }else{
-     alert("Please write a city");
- }
-
-//we need to find the value of search.value and === to city from the result. 
-}
-*/
-
-//Challenge 1- Get current date
-//so this one shows the full date and whenever you want to get something do now.getmonth
-
-
-
-//i changed the value of h2 back to city!!! sorry lol
-
-
-//finds current city
-//let form = document.querySelector("form");
-//form.addEventListener("submit", citySearch);
-
-
-
-/*
-
-axios.get(`${apiUrl}&appid=${apiKey}`).then(showTemp);
-axios.get(`${apiUrl}&appid=${apiKey}`).then(showDate);
-axios.get(`${apiUrl}&appid=${apiKey}`).then(showHumidity);
-axios.get(`${apiUrl}&appid=${apiKey}`).then(showPressure);
-axios.get(`${apiUrl}&appid=${apiKey}`).then(showTemperatureDescription);
-//axios.get(`${apiUrl}&appid=${apiKey}`).then(showTeperature);
-axios.get(`${apiUrl}&appid=${apiKey}`).then(windSpeed);
-axios.get(`${apiUrl}&appid=${apiKey}`).then(showDate);
-axios.get(`${apiUrl}&appid=${apiKey}`).then(temperatureIcon);
-*/
-//come back to this
-//let button= document.querySelector("button");
-//button.addEventListener("click", getCurrentPosition);
 function search(city){
     let apiKey = "ac254995f1530b05133bdf3b89d170a4";
     let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
     axios.get(apiUrl).then(displayTemperature);
+
+    apiUrl=`https://api.openweathermap.org/data/2.5/forecast?q=${city}&appid=${apiKey}&units=metric`;
+    axios.get(apiUrl).then(displayForecast);
+
 
 }
 function handleSearch(event){
